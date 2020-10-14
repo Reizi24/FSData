3c64aa464dc81a5c25e4c8246e0c7295f7b356e0
Reizi24 <72852863+Reizi24@users.noreply.github.com>
2020-10-14 06:18:44 -0700

    Add files via upload

---
+++ FSDProbabilitas.R
@@ -0,0 +1,9 @@
+#SOAL Variabel Acak Binomial#
+
+# Misalnya, Riko menendang bola kegawang sebanyak 20 kali
+# X adalah frekuensi Anda mendapatkan 5 kali gol
+# dan probabilitas keberhasilannya adalah 1/5 di masing-masing dari 20 uji coba menendang bola.
+# Berapa probabilitasnya
+# bahwa kita mendapatkan X = 5 gol, dan sukses dari 20 percobaan?
+
+dbinom(x = 5, size = 20, prob = 1/5)
